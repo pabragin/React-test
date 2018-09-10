55b1ef8fe910ae350f54b179bd0649d2b5694085
Pavel Bragin <bragin_pa@soshace.com>
2018-09-10 22:43:25 +0300

    aboutComponent added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Footer from './FooterComponent'
 import Home from './HomeComponent'
 import {Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -49,6 +50,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage}/>
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
                     <Route exact path='/contactus' component={Contact} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Redirect to="/home" />
