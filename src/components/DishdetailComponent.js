79d1529aaa1eb45f0222d155e5d2fe3f87684172
Pavel Bragin <bragin_pa@soshace.com>
2018-09-16 20:33:02 +0300

    assiggnment 3 complete

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import React from "react";
+import CommentForm from './CommentFormComponent';
 
     function RenderDish({dish}) {
         return(
@@ -31,6 +32,7 @@ import React from "react";
                     <ul className="list-unstyled">
                         {commentsView}
                     </ul>
+                    <CommentForm/>
                 </div>
             );
         }
