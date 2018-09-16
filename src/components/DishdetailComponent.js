bf0c47accd3a7b0a0e3d3cfec73f81eaf3f32e6a
Pavel Bragin <bragin_pa@soshace.com>
2018-09-16 21:33:52 +0300

    action implemented

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,7 +28,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -109,7 +110,7 @@ class CommentForm extends Component {
         );
     }
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}) {
         if (comments != null) {
             const commentsView = comments.map((comment) => {
                 return (
@@ -125,7 +126,7 @@ class CommentForm extends Component {
                     <ul className="list-unstyled">
                         {commentsView}
                     </ul>
-                    <CommentForm/>
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </div>
             );
         }
@@ -156,7 +157,10 @@ class CommentForm extends Component {
                             <RenderDish dish={props.dish} />
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={props.comments} />
+                            <RenderComments comments={props.comments}
+                                            addComment={props.addComment}
+                                            dishId={props.dish.id}
+                            />
                         </div>
                     </div>
                 </div>
